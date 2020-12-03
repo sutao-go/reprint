833e326a45958d361de33eeadaa924bc75520169
sutao-go <1565863804@qq.com>
2020-12-03 21:53:45 +0800

    复刻代码第一次提交

---
+++ src/main/java/com/imooc/reprint/ReprintApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.reprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReprintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReprintApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imooc/reprint/ReprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.reprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReprintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
